8b86cad23d8409c78b3b7d54f1c6bdc465bf03ca
EricBrisola <eric.coca13@gmail.com>
2024-01-24 02:30:43 -0300

    added projects main function

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [allTransactions, setAllTransactions] = useState([]);
+  const [transaction, setTransaction] = useState({
+    value: "",
+    type: "deposit",
+  });
+
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+    setAllTransactions((prev) => [...prev, transaction]);
+    console.log(transaction);
+    setTransaction({ value: "", type: "deposit" });
+  };
+
+  const addNewTransaction = (ev) => {
+    setTransaction({ ...transaction, [ev.target.name]: ev.target.value });
+  };
+
+  console.log(allTransactions);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>Controle de transações</h2>
+
+      <form action="" method="post" onSubmit={handleSubmit}>
+        <label htmlFor="transaction-value">Valor: </label>
+        <input
+          type="number"
+          name="value"
+          id="transaction-value"
+          onChange={addNewTransaction}
+          value={transaction.value}
+        />
+        <select
+          id="choice"
+          name="type"
+          value={transaction.type}
+          onChange={addNewTransaction}
+        >
+          <option value="deposit">Depositar</option>
+          <option value="remove">Retirar</option>
+        </select>
+
+        <button type="submit">Adicionar</button>
+      </form>
+
+      <section className="transactions">
+        {allTransactions.length > 0 ? (
+          allTransactions.map((el) => (
+            <div className="transaction" key={Math.floor(Math.random() * 100)}>
+              <p>Valor: {el.value}</p>
+              <p>Tipo: {el.type}</p>
+            </div>
+          ))
+        ) : (
+          <p>Sem transações feitas</p>
+        )}
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
