5811f074d698f4902ac74948c14b001de1181b26
EricBrisola <eric.coca13@gmail.com>
2024-01-28 01:49:19 -0300

    added POST and DELETE methods, created a database using json-server

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,61 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
 function App() {
   const [allTransactions, setAllTransactions] = useState([]);
+  const [transactionToPut, setTransactionToPut] = useState({});
   const [transaction, setTransaction] = useState({
     value: "",
     type: "deposit",
     id: "",
   });
 
+  const selectUiController = useRef(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/transactions")
+      .then((resp) => resp.json())
+      .then((json) => setAllTransactions(json));
+  }, []);
+
   const handleSubmit = (ev) => {
     ev.preventDefault();
     setAllTransactions((prev) => [...prev, transaction]);
+    postToTransactions(transaction);
     setTransaction({ ...transaction, value: "" });
+
+    if (selectUiController.current) {
+      selectUiController.current.value = "deposit";
+    }
   };
 
   const addNewTransaction = (ev) => {
     setTransaction({
       ...transaction,
       [ev.target.name]: ev.target.value,
-      id:
-        allTransactions.length === 0
-          ? 1
-          : allTransactions[allTransactions.length - 1].id + 1,
+      id: "" + Math.floor(Math.random() * 100000),
     });
   };
+
+  const postToTransactions = async (transaction) => {
+    await fetch("http://localhost:3000/transactions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(transaction),
+    });
+  };
+
+  async function deleteTransaction(id) {
+    await fetch(`http://localhost:3000/transactions/${id}`, {
+      method: "DELETE",
+    });
+
+    setAllTransactions((prev) =>
+      prev.filter((transaction) => transaction.id !== id)
+    );
+  }
 
   const totalBalance =
     allTransactions.length >= 1
@@ -42,7 +73,11 @@ function App() {
     <>
       <h2>Controle de transações</h2>
 
-      <form action="" method="post" onSubmit={handleSubmit}>
+      <form
+        // action="http://localhost:3000/transactions"
+        method="post"
+        onSubmit={handleSubmit}
+      >
         <label htmlFor="transaction-value">Valor: </label>
         <input
           type="number"
@@ -50,16 +85,22 @@ function App() {
           id="transaction-value"
           onChange={addNewTransaction}
           value={transaction.value}
+          required={true}
+          min={0}
         />
-        <select
-          id="choice"
-          name="type"
-          value={transaction.type}
-          onChange={addNewTransaction}
-        >
-          <option value="deposit">Depositar</option>
-          <option value="remove">Sacar</option>
-        </select>
+        <label htmlFor="choice">
+          Tipo:
+          <select
+            id="choice"
+            name="type"
+            value={transaction.type}
+            onChange={addNewTransaction}
+            ref={selectUiController}
+          >
+            <option value="deposit">Depositar</option>
+            <option value="remove">Sacar</option>
+          </select>
+        </label>
 
         <button type="submit">Adicionar</button>
       </form>
@@ -71,7 +112,7 @@ function App() {
               <p>Valor: R$ {el.value}</p>
               <p>Tipo: {el.type === "deposit" ? "Depósito" : "Saque"}</p>
               <button>Atualizar</button>
-              <button>Deletar</button>
+              <button onClick={() => deleteTransaction(el.id)}>Deletar</button>
             </div>
           ))
         ) : (
